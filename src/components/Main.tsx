6f8078f33b1b087561baf3eab71275c7e7436d18
Techatach <techatach@gmail.com>
2024-07-26 12:32:48 +0700

    first commit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -37,7 +37,9 @@ export default function Main() {
             <a href="/evaluation" className="mr-5 hover:text-gray-900">
               การประเมินนโยบายสาธารณะ
             </a>
-            <a className="mr-5 hover:text-gray-900">ChatGPT</a>
+            <a href="/chatgpt" className="mr-5 hover:text-gray-900">
+              ChatGPT
+            </a>
             <a className="mr-5 hover:text-gray-900">ติดต่อ</a>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 ">
